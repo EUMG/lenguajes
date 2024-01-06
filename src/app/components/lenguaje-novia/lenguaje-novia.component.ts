790bcf7aec6ab218d465889f9a946f26f12c52cb
EUMG <edgar.molina@coppel.com>
2024-01-05 17:51:01 -0700

    Se suben más

--- src/app/components/lenguaje-novia/lenguaje-novia.component.ts
+++ src/app/components/lenguaje-novia/lenguaje-novia.component.ts
@@ -18,18 +18,29 @@ export class LenguajeNoviaComponent {
     for(let i=0; i < inputText.length; i++){
         const img = document.createElement('img');
         if(inputText[i].trim() !== '' ){
-          img.src = `../assets/abecedario/${inputText[i]}.jpg`;
+          img.src = `../../../assets/abecedario/novia/${inputText[i]}.jpg`;
           img.onerror = function() {
-            img.src = `../assets/abecedario/especial.jpg`;
+            img.src = `../../../assets/abecedario/novia/especial.jpg`;
           };
           img.alt = `${inputText}`;
           document.querySelector('.images')?.appendChild(img);
         }
         else{
-          img.src = `../assets/abecedario/blanco.jpg`;
+          img.src = `../../../assets/abecedario/novia/blanco.jpg`;
           img.alt = `espacio`;
           document.querySelector('.images')?.appendChild(img);
         }
     }
+  }
+  importar(e : any) : void {
+    const archivo = e.target.files[0];
+    console.log(archivo);
+    const leer = new FileReader();
+    leer.onload = (e: any) => {
+      const img = document.createElement('img');
+      img.src = e.target.result;
+      document.querySelector('.inversa')?.appendChild(img);
+    };
+    leer.readAsDataURL(archivo);
   }
 }
